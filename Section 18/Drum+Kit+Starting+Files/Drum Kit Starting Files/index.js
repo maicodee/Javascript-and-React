fdba47cb12cfa38a1214e22e6940a4d49dc86c09
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2025-03-10 11:20:42 +0530

    first

---
+++ Section 18/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,27 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrumButtons; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+     
+switch buttonInnerHTML = this.innerHTML;{
+    case value:
+        
+        break;
+
+    default:
+        break;
+}    
+    
+});
+}
+
+
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//     audio.play();
+// this.style.color = "white";
